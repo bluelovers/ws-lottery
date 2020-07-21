bef9adfc84c6f15d305be9225f02c0f7a91e1851
bluelovers <codelovers@users.sourceforge.net>
2020-07-22 01:19:13 +0800

    .

--- packages/random-lotto/test/weight.ts
+++ packages/random-lotto/test/weight.ts
@@ -2,8 +2,11 @@ import superlotto638 from '@lazy-lotto/tw-history-data/lib/data/superlotto638.js
 import { IRecordRow, IResultSuperlotto638 } from '@lazy-lotto/types';
 import { randomLottoGenerator, IRandomLottoParams } from '../index';
 import fill from 'fill-range';
+import naturalCompare from '@bluelovers/string-natural-compare';
 
-const weightTable = (Object.values(superlotto638) as IRecordRow<IResultSuperlotto638>[])
+let historyArray = (Object.values(superlotto638) as IRecordRow<IResultSuperlotto638>[]);
+
+const weightTable = historyArray
 	.reduce((a, row) =>
 	{
 
@@ -12,6 +15,8 @@ const weightTable = (Object.values(superlotto638) as IRecordRow<IResultSuperlott
 			a[0][v] ??= 1;
 			a[0][v]++;
 		})
+
+		row.result[0] = row.result[0].sort(naturalCompare);
 
 		let v2 = row.result[1];
 
@@ -38,9 +43,69 @@ let g = randomLottoGenerator({
 	sortResults: true,
 })
 
-for (let i = 0; i < 15; i++)
+let list = [];
+
+for (let i = 0; i < 100; i++)
 {
 	let actual = g.next().value;
 
-	console.log(actual)
+	let m = simpleMatchIn(actual[0], historyArray)
+
+	if (m !== void 0)
+	{
+		list.push(m);
+
+//		console.dir(m, {
+//			depth: null,
+//		})
+	}
+
+	//console.log(actual)
+}
+
+list.sort((a, b) => {
+	return b.max - a.max
+})
+
+console.dir(list, {
+	depth: null,
+})
+
+function simpleMatchInArray<T extends number[]>(a1: T, a2: T)
+{
+	return a2.filter(v => a1.includes(v))
+}
+
+function simpleMatchIn<T extends number[]>(a1: T,historyArray: IRecordRow<[
+	T,
+	...any
+]>[])
+{
+	let bool: boolean
+	let max: number = 0;
+
+	let ls = historyArray.reduce((a, v) => {
+
+		let m = simpleMatchInArray(a1, v.result[0]);
+
+		if (m.length > 3)
+		{
+			a[m.length] ??= [];
+			a[m.length].push(v.result[0])
+
+			max = Math.max(m.length, max)
+
+			bool = true;
+		}
+
+		return a
+	}, {} as Record<string, T[]>)
+
+	if (bool === true)
+	{
+		return {
+			max,
+			ls
+		}
+	}
 }
